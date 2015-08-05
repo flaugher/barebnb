4aa146b5d55c0799a16bd775fd40066162e1f420
Robert Flaugher <rlfprog@gmail.com>
2015-08-05 14:59:22 -0700

    Added search form and sample data

--- search.js
+++ search.js
@@ -1,1 +1,51 @@
-// AngularJS goes here
+var props = [
+    {
+        city: 'Los Angeles',
+        country: 'United States',
+        beds: '2',
+        baths: '2',
+        amenities: 'Hot tub, flat screen TV'
+    },
+    {
+        city: 'New York',
+        country: 'United States',
+        beds: '1',
+        baths: '1.5',
+        amenities: 'Great view, '
+    },
+    {
+        city: 'Chicago',
+        country: 'United States',
+        beds: '3',
+        baths: '3',
+        amenities: 'Fireplace'
+    },
+    {
+        city: 'London',
+        country: 'UK',
+        beds: '2',
+        baths: '2.5',
+        amenities: 'Fireplace, parking'
+    },
+    {
+        city: 'Paris',
+        country: 'France',
+        beds: '1',
+        baths: '1.5',
+        amenities: 'Swimming pool, Wi-fi'
+    },
+    {
+        city: 'Hong Kong',
+        country: 'China',
+        beds: '2',
+        baths: '2',
+        amenities: 'Wi-fi, parking'
+    },
+    {
+        city: 'Sao Paulo',
+        country: 'Brazil',
+        beds: '2',
+        baths: '2',
+        amenities: 'Beach access'
+    }
+]
